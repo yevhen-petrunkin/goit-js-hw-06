b966c3d699a94b11be89dc709decfb64c73a8fd4
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-02 21:27:15 +0200

    Complete Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const updatedIngredients = ingredients.map(ingredient => {
+  const ingredientRef = document.createElement('li');
+  ingredientRef.textContent = ingredient;
+  ingredientRef.classList.add('item');
+  return ingredientRef;
+});
+
+ingredientsRef.append(...updatedIngredients);
+
+console.log(ingredientsRef);
