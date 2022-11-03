c0227d113c1df0e0f07cb193bd6cbd725bd25a9e
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-03 14:38:32 +0200

    Complete Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  return formData;
+}
